027bb7a8dd8c84a440bf8851b66930694b0cd94c
JineshVi <76246413+JineshVi@users.noreply.github.com>
2023-03-02 20:41:14 +0530

    Add files via upload

---
+++ engines/capulet_engine.py
@@ -0,0 +1,9 @@
+class CapuletEngine():
+    def __init__(self, last_service_mileage, current_mileage):
+        self.last_service_mileage = last_service_mileage
+        self.current_mileage = current_mileage
+
+    def needs_service(self):
+        mileage_limit = self.current_mileage - self.last_service_mileage
+
+        return mileage_limit > 30000
