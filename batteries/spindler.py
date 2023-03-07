33aa7cefa7e8da4f75934de0755ba51cf41ecceb
JineshVi <76246413+JineshVi@users.noreply.github.com>
2023-03-07 19:21:47 +0530

    Update spindler.py

--- batteries/spindler.py
+++ batteries/spindler.py
@@ -1,7 +1,11 @@
+from batteries.battery import Battery
+from datetime import datetime, date, time
+
 class Spindler():
     def __init__(self, last_service_date, current_date):
         self.last_service_date = last_service_date
         self.current_date = current_date
 
     def needs_service(self):
-        return self.current_date - self.last_service_date > 2
+        due_date = self.last_service_date.replace(year=self.last_service_date.year + 2)
+        return self.current_date > due_date
