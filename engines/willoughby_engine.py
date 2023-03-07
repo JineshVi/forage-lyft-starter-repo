ca18accea7c67dfd6ef449ac5131dbbb7e2d6acc
JineshVi <76246413+JineshVi@users.noreply.github.com>
2023-03-07 19:18:37 +0530

    Update willoughby_engine.py

--- engines/willoughby_engine.py
+++ engines/willoughby_engine.py
@@ -1,4 +1,6 @@
-class WilloughbyEngine():
+from engines.engine import Engine
+
+class WilloughbyEngine(Engine):
     def __init__(self, last_service_mileage, current_mileage):
         self.last_service_mileage = last_service_mileage
         self.current_mileage = current_mileage
