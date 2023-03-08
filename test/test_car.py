d297e942d3081c650c4f5350221e6426615d86c5
JineshVi <76246413+JineshVi@users.noreply.github.com>
2023-03-08 14:17:22 +0530

    Update test_car.py

--- test/test_car.py
+++ test/test_car.py
@@ -10,6 +10,10 @@ from ..engines import sterman_engine
 from ..batteries import nubbin
 from ..batteries import spindler
 
+#importing tire modules and functions
+from ..tires import carrigan
+from ..tires import octoprime
+
 
 class Testing_engines(unittest.TestCase):
     #Engine Testing
@@ -69,7 +73,7 @@ class Testing_engines(unittest.TestCase):
         self.assertFalse(car_battery.needs_service())
 
     def test_spindler_battery_1(self):
-        last_service_date = date(2020, 6, 7)
+        last_service_date = date(2019, 6, 7)
         current_date = date.today()
 
         car_battery = spindler.Spindler(last_service_date, current_date)
@@ -81,6 +85,31 @@ class Testing_engines(unittest.TestCase):
 
         car_battery = spindler.Spindler(last_service_date, current_date)
         self.assertFalse(car_battery.needs_service())
+        
+    #Tire Testing
+    def test_carrigan_tire_1(self):
+        worn_out_tires = [0.9,0.4,0.5,0.9]
+
+        car_tire = carrigan.Carrigan(worn_out_tires)
+        self.assertTrue(car_tire.needs_service())
+
+    def test_carrigan_tire_2(self):
+        worn_out_tires = [0.5,0.4,0.5,0.4]
+
+        car_tire = carrigan.Carrigan(worn_out_tires)
+        self.assertFalse(car_tire.needs_service())
+
+    def test_octoprime_tire_1(self):
+        worn_out_tires = [0.9,0.8,0.7,0.9]
+
+        car_tire = octoprime.Octoprime(worn_out_tires)
+        self.assertTrue(car_tire.needs_service())
+
+    def test_octoprime_tire_2(self):
+        worn_out_tires = [0.2,0.2,0.4,0.3]
+
+        car_tire = octoprime.Octoprime(worn_out_tires)
+        self.assertFalse(car_tire.needs_service())
     
 
 if __name__ == '__main__':
